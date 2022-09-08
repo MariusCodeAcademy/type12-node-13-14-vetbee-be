e96dbdb7456de6f5e14be667f4d6dcd17f7bd2e1
Marius <marius@example.com>
2022-09-08 09:44:21 +0300

    init sql tables

---
+++ src/server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+// susikuriam serveri
+const express = require('express');
+// eslint-disable-next-line no-unused-vars
+const colors = require('colors');
+const morgan = require('morgan');
+const mysql = require('mysql2/promise');
+const dbConfig = require('./config');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(morgan('dev'));
+// prisidedam morgan/cors
+// GET / - msg: server online
+
+app.get('/', (req, res) => {
+  res.json({
+    msg: 'Server online',
+  });
+});
+
+// ROUTES
+
+// 404 - returns json
+app.use((req, res) => {
+  res.status(404).json({
+    msg: 'Page not found',
+  });
+});
+
+async function testDbConnection() {
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    const [rows] = await conn.query('SELECT 1');
+    // console.log('rows ===', rows);
+    console.log('Connected to MYSQL DB '.bgCyan.bold);
+    conn.end();
+  } catch (error) {
+    console.log(`Error connecting to db ${error.message}`.bgRed.bold);
+    // console.log('error ===', error);
+    if (error.code === 'ECONNREFUSED') {
+      console.log('is Xammp running?'.yellow);
+    }
+  }
+}
+
+testDbConnection();
+
+app.listen(port, () => console.log(`Server online on port ${port}`.bgYellow.bold));
